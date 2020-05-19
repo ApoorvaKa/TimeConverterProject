d634ef92f9069884d1f3b6a4a7843c8fa47bfb9c
ApoorvaKa <42742772+ApoorvaKa@users.noreply.github.com>
2020-05-19 15:01:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TimeConverterNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeConverter.ConvertToEastern("13:00", "Pacific Standard Time");
+            TimeConverter.IsTodayInList("0,1,2,3,4");
+        }
+
+    }
+
+}
---
+++ TimeConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeConverterNS
+{
+    class TimeConverter
+    {
+        public static DateTime? ConvertToEastern(string user_input_time, string user_input_zone)
+        {
+
+            DateTime user_time;
+            DateTime.TryParse(user_input_time, out user_time);
+            TimeZoneInfo user_timezone = TimeZoneInfo.FindSystemTimeZoneById(user_input_zone);
+            TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+            if (user_time.Equals(DateTime.MinValue))
+            {
+                Console.WriteLine("This is an invalid time");
+                return null;
+            }
+
+            else
+            {
+                DateTime estTime = TimeZoneInfo.ConvertTime(user_time, user_timezone, est);
+                return estTime;
+            }
+
+        }
+
+        public static bool IsTodayInList(string user_days)
+        {
+            List<string> list_of_days_string = user_days.Split(',').ToList();
+            List<int> list_of_days = list_of_days_string.Select(int.Parse).ToList();
+            DayOfWeek today = DateTime.Now.DayOfWeek;
+            var num = (int)today;
+
+            return list_of_days.Contains(num);
+        }
+    }
+}
+
