9c6baf7bd1a0230eff34eeacdedad93f39a6b667
ApoorvaKa <42742772+ApoorvaKa@users.noreply.github.com>
2020-07-22 19:47:51 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-﻿namespace TimeConverterNS
+﻿using System;
+
+namespace TimeConverterNS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TimeConverter.ConvertToEastern("13:00", "Pacific Standard Time");
-            TimeConverter.IsTodayInList("0,1,2,3,4");
+            Console.WriteLine("Hello World");
+            DateTime survey = new DateTime(2010, 6, 13, 18, 32, 0);
+            DateTime response = new DateTime(2010, 6, 13, 20, 01, 34);
+            TimeSpan start = new TimeSpan(7, 0, 0);
+            TimeSpan end = new TimeSpan(18, 0, 0);
+
+            Console.WriteLine(TimeConverter.ConvertToEastern("00:00", "Mountain Standard Time"));
+            Console.WriteLine(TimeConverter.IsTodayInList("0,1,2,3,4"));
+            Console.WriteLine(TimeConverter.IsMessageRespondingToSurvey(survey, response));
+            Console.WriteLine(TimeConverter.GenerateRandomTime(start, end));
         }
 
     }
